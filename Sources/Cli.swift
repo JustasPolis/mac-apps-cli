dfd3557b2eab72cd09d7735f6be9e755e6848331
JustasPolis <j.polikevicius@gmail.com>
2024-05-19 12:53:41 +0300

    fixes

--- Sources/Cli.swift
+++ Sources/Cli.swift
@@ -1,7 +1,7 @@
 import ArgumentParser
 
 @main
-struct Cli: ParsableCommand {
+struct App: ParsableCommand {
   static let configuration = CommandConfiguration(
     abstract: "MacOS apps CLI tool",
     subcommands: [List.self, Open.self]
